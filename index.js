ea03828fe785212ac1d6ce27c76459fef3531efd
joshex150 <joshex150@gmail.com>
2023-06-14 14:31:23 +0100

    nunu

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:5173'
+  origin: 'https://huddle-git-main-joshex150.vercel.app/profile'|| '*'
 }));
 app.use(express.static('public'));
 
