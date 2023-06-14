acf6230f3af0d83aa62a3b817a53502e8ad9e0ec
joshex150 <joshex150@gmail.com>
2023-06-14 14:32:06 +0100

    yeah

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: 'https://huddle-git-main-joshex150.vercel.app/profile'|| '*'
+  origin: 'http://127.0.0.1:5173'|| 'https://huddle-git-main-joshex150.vercel.app'
 }));
 app.use(express.static('public'));
 
