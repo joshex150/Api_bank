c1cd51b38eb3a80d3d8cbc5ed77dcc3cf8ffb9a0
joshex150 <joshex150@gmail.com>
2023-06-14 14:39:08 +0100

    nunu

--- controllers/checkTransactions.js
+++ controllers/checkTransactions.js
@@ -4,7 +4,12 @@ module.exports = async function (req, res) {
   try {
     const email = req.body.email;
     const transactionData = await invoiceCollection.find({userRef:email});
-    res.send(transactionData)
+    if (transactionData) {
+      res.send(transactionData) // Send transactionData as JSON
+    } else {
+      console.error("Failed to find transaction data");
+      res.sendStatus(404);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal server error');
