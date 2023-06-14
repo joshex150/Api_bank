e8d51d99df3fa2babceceaf313f2ae1eb51c5ac9
joshex150 <joshex150@gmail.com>
2023-06-14 13:55:27 +0100

    try

--- controllers/checkTransactions.js
+++ controllers/checkTransactions.js
@@ -3,7 +3,7 @@ const invoiceCollection = require('../models/invoiceModel');
 module.exports = async function (req, res) {
   try {
     const { email } = req.body.email;
-    const transactionData = await invoiceCollection.find(email);
+    const transactionData = await invoiceCollection.find({userRef:email});
     res.send(transactionData)
   } catch (error) {
     console.error(error);
