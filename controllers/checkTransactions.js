6c402acb899559ed955978f1814947a74a22fa94
joshex150 <joshex150@gmail.com>
2023-06-14 12:53:51 +0100

    new

--- controllers/checkTransactions.js
+++ controllers/checkTransactions.js
@@ -1,10 +1,16 @@
 const invoiceCollection = require('../models/invoiceModel');
 
-module.exports = async function(req, res) {
+module.exports = async function (req, res) {
   try {
     const { email } = req.body;
     const transactionData = await invoiceCollection.findOne({ email: email });
-    res.send(transactionData)
+
+    if (transactionData) {
+      res.send(transactionData);
+    } else {
+      console.error("Failed to find transaction data");
+      res.sendStatus(404);
+    }
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
